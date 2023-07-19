cdb819bfff5c4775db9be9baa95c791e2aaa956f
stewest <stew@sparksinteractive.co.nz>
2023-07-20 10:35:37 +1200

    attempt module xports

---
+++ packages/eslint/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2018, // Put whatever version you want here
+  },
+  env: {
+    browser: true,
+  },
+  settings: {
+    next: {
+      rootDir: ['src/*/'],
+    },
+  },
+};
